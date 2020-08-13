ad4b4e10f28178b1a7c239602c2035251c348d75
mmekut-mfon.edet <mmekut-mfon.edet@interswitchgroup.com>
2020-08-13 02:51:09 +0100

    antd setup with customise theme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+//import 'antd/dist/antd.css';
+import { Button } from "antd";
+import "./App.less";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      Blog design.
-   
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/* <header className="App-header">Blog design.</header> */}
+            <div>
+                <Button type="primary">Button</Button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
